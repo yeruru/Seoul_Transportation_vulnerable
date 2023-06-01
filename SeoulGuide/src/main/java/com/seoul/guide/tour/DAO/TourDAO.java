fdecf3c8f6973456a5d0686a14d68833e81c7dcc
cjstmdgusqw <cjstmdgusqw@gmail.com>
2023-06-01 09:35:17 +0900

    FEAT : tour.jsp 상세 검색기능 구현 완료

--- SeoulGuide/src/main/java/com/seoul/guide/tour/DAO/TourDAO.java
+++ SeoulGuide/src/main/java/com/seoul/guide/tour/DAO/TourDAO.java
@@ -1,10 +1,11 @@
 package com.seoul.guide.tour.DAO;
 
 import java.util.List;
+import java.util.Map;
 
 import com.seoul.guide.tour.DTO.TourDTO;
 
 public interface TourDAO {
 	public List<TourDTO> selecttourlist() throws Exception;
-
+	public List<TourDTO> detailSearch(Map<String, Object> param) throws Exception;
 }
--- SeoulGuide/src/main/java/com/seoul/guide/tour/Service/TourServiceImpl.java
+++ SeoulGuide/src/main/java/com/seoul/guide/tour/Service/TourServiceImpl.java
@@ -1,6 +1,10 @@
 package com.seoul.guide.tour.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +20,17 @@ public class TourServiceImpl implements TourService {
 	@Override
 	public List<TourDTO> gettourList() throws Exception {
 		return tourDAO.selecttourlist();
+	}
+
+	@Override
+	public List<TourDTO> detailSearch(String[] menu_icon, String tourist_subtitle) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		List<String> list = Arrays.asList(menu_icon);
+		System.out.println(list);
+		map.put("menu_icon", list);
+		map.put("tourist_subtitle", tourist_subtitle);
+		System.out.println(map.get("tourist_subtitle"));
+		return tourDAO.detailSearch(map);
 	}
 
 }
