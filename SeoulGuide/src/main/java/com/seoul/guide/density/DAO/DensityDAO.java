23a4aeef4fa5477d5fa8bbde97da5ab81b95095b
Likey7 <82503893+Likey7@users.noreply.github.com>
2023-06-09 14:38:37 +0900

    feat

--- SeoulGuide/src/main/java/com/seoul/guide/density/DAO/DensityDAO.java
+++ SeoulGuide/src/main/java/com/seoul/guide/density/DAO/DensityDAO.java
@@ -1,13 +1,18 @@
 package com.seoul.guide.density.DAO;
 
 import java.util.List;
-
 import com.seoul.guide.density.DTO.DensityDTO;
 
+//DensityDAO
 public interface DensityDAO {
-    List<DensityDTO> selectAllDensity() throws Exception;
-    void updateDensity(DensityDTO densityDTO) throws Exception;
-    List<DensityDTO> selectAllDensityOrderByDenseLvl() throws Exception;
-    List<DensityDTO> selectAllDensityOrderByName() throws Exception;
-    List<DensityDTO> selectAllDensityUnsorted() throws Exception; // 추가
+  void updateDensity(DensityDTO densityDTO) throws Exception;
+  List<DensityDTO> selectAllDensityOrderByName(int offset, int rowsPerPage) throws Exception;
+  List<DensityDTO> selectAllDensityOrderByDenseLvl(int offset, int rowsPerPage) throws Exception;
+  List<DensityDTO> selectAllDensityUnsorted(int offset, int rowsPerPage) throws Exception; // 수정
+  
+  //페이지네이션 코드 
+  int getTotalDensityCount() throws Exception;
+  List<DensityDTO> selectAllDensityForUpdate() throws Exception;
+
+  
 }
